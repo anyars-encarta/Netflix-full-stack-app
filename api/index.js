7e2385f7148bebd54c7785530e0695f01ad46528
anyars-encarta <anyarsencarta@gmail.com>
2024-05-09 14:30:33 +0000

    Create DB Connection in index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
 const app = express();
+
+dotenv.config();
+
+mongoose.connect(
+    process.env.MONGO_URL
+).then(
+    () => console.log('DB Connection successful')
+).catch((e) => console.log(e));
 
 app.listen(5000, () => {
     console.log('You are re-building Netflix and your server is running!')
-})
+});
