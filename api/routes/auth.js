201c127b4d6c4a2c31e718b9f83ee2df530a1378
anyars-encarta <anyarsencarta@gmail.com>
2024-05-11 11:07:33 +0000

    Encrypt password

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const CryptoJS = require("crypto-js");
 const User = require("../models/User.js");
 
 //REGSITER
@@ -6,7 +7,11 @@ router.post("/register", async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
+        // password: req.body.password,
+        // Encrypt
+        password: CryptoJS.AES.encrypt(
+            req.body.password, process.env.SECRET_KEY
+        ).toString(),
     });
 
     try {
