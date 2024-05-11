4d1eb5bd15fcb1b1335d5bc1103212a9ad32c5bb
anyars-encarta <anyarsencarta@gmail.com>
2024-05-11 18:12:59 +0000

    Delete a movie

--- api/routes/movies.js
+++ api/routes/movies.js
@@ -42,4 +42,20 @@ router.put("/:id", verify, async (req, res) => {
     }
 });
 
+// DELETE
+router.delete("/:id", verify, async (req, res) => {
+    if (req.user.isAdmin) {
+        try {
+            await Movie.findByIdAndUpdate(req.params.id);
+
+            res.status(200).json("Movie deleted successfully!");
+        } catch (e) {
+            res.status(500).json(e)
+        }
+
+    } else {
+        res.status(403).json('You are not allowed to delete movies!');
+    }
+});
+
 module.exports = router;
