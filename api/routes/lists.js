faa16e3031156680c6085201ebdb65001d1b1701
anyars-encarta <anyarsencarta@gmail.com>
2024-05-11 19:33:07 +0000

    Get List

--- api/routes/lists.js
+++ api/routes/lists.js
@@ -20,7 +20,46 @@ router.post("/", verify, async (req, res) => {
 });
 
 // DELETE
+router.delete("/:id", verify, async (req, res) => {
+    if (req.user.isAdmin) {
+        try {
+            await List.findByIdAndDelete(req.params.id);
+            res.status(201).json("List deleted successfully");
+        } catch (e) {
+            res.status(500).json(e)
+        }
 
-// GET
+    } else {
+        res.status(403).json('You are not allowed to delete lists!');
+    }
+});
 
+// GET LISTS
+router.get("/", verify, async (req, res) => {
+    const typeQuery = req.query.type;
+    const genreQuery = req.query.genre;
+    let list = [];
+
+    try {
+        if (typeQuery) {
+            if (genreQuery) {
+                list = await List.aggregate([
+                    { $sample: { size: 10 } },
+                    { $match: { type: typeQuery, genre: genreQuery } },
+                ]);
+            } else {
+                list = await List.aggregate([
+                    { $sample: { size: 10 } },
+                    { $match: { type: typeQuery } },
+                ])
+            }
+        } else {
+            list = await List.aggregate([{ $sample: { size: 10 } }])
+        }
+
+        res.status(200).json(list);
+    } catch (e) {
+        res.status(500).json(e)
+    }
+});
 module.exports = router;
