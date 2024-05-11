3a72b962202018b30c606edeaf69580490922291
anyars-encarta <anyarsencarta@gmail.com>
2024-05-11 18:10:37 +0000

    Create a new movie

---
+++ api/routes/movies.js
@@ -0,0 +1,45 @@
+const router = require("express").Router();
+const verify = require("../verifyToken");
+const Movie = require("../models/Movie");
+
+
+// CREATE
+router.post("/", verify, async (req, res) => {
+    if (req.user.isAdmin) {
+        const newMovie = new Movie(req.body);
+
+        try {
+            const savedMovie = await newMovie.save();
+            res.status(201).json(savedMovie);
+        } catch (e) {
+            res.status(500).json(e)
+        }
+
+    } else {
+        res.status(403).json('You are not allowed to create movies!');
+    }
+});
+
+// UPDATE
+router.put("/:id", verify, async (req, res) => {
+    if (req.user.isAdmin) {
+        try {
+            const updatedMovie = await Movie.findByIdAndUpdate(
+                req.params.id,
+                {
+                    $set: req.body,
+                },
+                { new: true }
+            );
+
+            res.status(200).json(updatedMovie);
+        } catch (e) {
+            res.status(500).json(e)
+        }
+
+    } else {
+        res.status(403).json('You are not allowed to create movies!');
+    }
+});
+
+module.exports = router;
