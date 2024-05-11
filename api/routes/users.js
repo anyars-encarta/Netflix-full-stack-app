c19f4a33864add09f3e3467d57a9103acb1b00d3
anyars-encarta <anyarsencarta@gmail.com>
2024-05-11 15:28:57 +0000

    Delete user

--- api/routes/users.js
+++ api/routes/users.js
@@ -25,7 +25,20 @@ router.put("/:id", verify, async (req, res) => {
         res.status(403).json('You can update only your account!');
     }
 });
+
 // DELETE
+router.delete("/:id", verify, async (req, res) => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+        try {
+            const updatedUser = await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("User has been deleted...")
+        } catch (e) {
+            res.status(500).json(e);
+        }
+    } else {
+        res.status(403).json('You can delete only your account!');
+    }
+});
 // GET SINGLE USER
 // GET ALL USERS
 // GET USER STATS
