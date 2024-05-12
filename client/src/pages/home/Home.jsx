3cfd118d4b114aeafc37a0bddcc2b2604e99303f
anyars-encarta <anyarsencarta@gmail.com>
2024-05-12 11:53:45 +0000

    Creating fetch list function

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,19 +2,27 @@ import Featured from '../../components/featured/Featured';
 import Navbar from '../../components/navbar/Navbar';
 import List from '../../components/list/List';
 import './home.scss';
+import { useEffect, useState } from 'react';
 
 const Home = ({ type }) => {
+  const [lists, setLists] =useState([]);
+
+  const getRandomLists = async () => {
+    try {
+      
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getRandomLists();
+  }, []);
+
   return (
     <div className='homeContainer'>
       <Navbar />
       <Featured type={type} />
-      <List />
-      <List />
-      <List />
-      <List />
-      <List />
-      <List />
-      <List />
       <List />
       <List />
       <List />
