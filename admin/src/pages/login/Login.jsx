bd32cc22140feb1b6eceb0a77893909b1ace92d1
anyars-encarta <anyarsencarta@gmail.com>
2024-05-19 12:59:02 +0000

    Implementing admin panel login

--- admin/src/pages/login/Login.jsx
+++ admin/src/pages/login/Login.jsx
@@ -1,12 +1,28 @@
+import { useContext, useState } from 'react';
 import './login.scss';
+import { AuthContext } from '../../context/userContext/AuthContext';
+import { login } from '../../context/userContext/apiCalls';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { isFetching, dispatch } = useContext(AuthContext);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    login({ email, password }, dispatch);
+  };
+
   return (
     <div className='login'>
-        <form className="loginForm">
-            <input type="email" placeholder='Email' className="loginInput" />
-            <input type="password" placeholder='Password' className="loginInput" />
-        </form>
+      <form className="loginForm">
+        <input type="email" placeholder='Email' className="loginInput" onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" placeholder='Password' className="loginInput" onChange={(e) => setPassword(e.target.value)} />
+        <button className='loginButton' onClick={handleLogin} disabled={isFetching}>
+          {isFetching ? 'Logging in...' : 'Login'}
+        </button>
+      </form>
     </div>
   )
 }
