1a79cd0834a423762a6c5a7a233571fce9333d89
anyars-encarta <anyarsencarta@gmail.com>
2024-05-21 13:23:16 +0000

    Correct firebase.js file

--- admin/src/firebase.js
+++ admin/src/firebase.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.envREACT_APP_MEASUREMENT_ID.
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
   };
 
   firebase.initializeApp(firebaseConfig);
